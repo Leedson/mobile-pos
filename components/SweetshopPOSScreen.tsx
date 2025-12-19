162151403198cec167915a03fbcd6ce0c177b313
Leedson <leetheleader@gmail.com>
2025-12-19 15:49:55 +0530

    googleint

--- components/SweetshopPOSScreen.tsx
+++ components/SweetshopPOSScreen.tsx
@@ -503,8 +503,8 @@ const styles = StyleSheet.create({
 
   alphaRow: { flexDirection: "row", marginBottom: 6 },
   alphaBtn: {
-    width: 50,
-    height: 50,
+    width: 65,
+    height: 65,
     alignItems: "center",
     justifyContent: "center",
     margin: 2,
